3410cb42ac3c078b2131a8a98eb060cfd76871c6
Fall1ngLight <keruloszabolcs1@gmail.com>
2021-11-11 11:30:33 +0100

    Corrected layout, added vuex/admin

--- client/src/components/Stats.vue
+++ client/src/components/Stats.vue
@@ -2,16 +2,18 @@
   <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4 mt-4">
     <div class="col">
       <div class="card h-100">
-        <div class="card-body d-flex align-items-center">
-          <div class="card-text">
-            <div class="row align-items-center">
-              <div class="col-4">
-                <i class="bi bi-people-fill big-icon ms-3 me-5"></i>
-              </div>
-              <div class="col-8 text-center">
-                <p class="lead m-0">21</p>
-                <p class="lead m-0">Total Users</p>
-              </div>
+        <div class="card-body">
+          <div class="row justify-content-center ">
+            <!-- center icon horizontally -->
+            <div class="col-4 d-flex justify-content-center align-items-center">
+              <i class="bi bi-people-fill big-icon"></i>
+            </div>
+            <div class="col-4 col-md-6 text-center d-flex align-items-center">
+              <p class="lead m-0">
+                21
+                <br />
+                Total Users
+              </p>
             </div>
           </div>
         </div>
@@ -19,16 +21,18 @@
     </div>
     <div class="col">
       <div class="card h-100">
-        <div class="card-body d-flex align-items-center">
-          <div class="card-text">
-            <div class="row align-items-center">
-              <div class="col-4">
-                <i class="bi bi-card-text big-icon ms-3 me-5"></i>
-              </div>
-              <div class="col-8 text-center">
-                <p class="lead m-0">122</p>
-                <p class="lead m-0">Total Notes</p>
-              </div>
+        <div class="card-body">
+          <div class="row justify-content-center ">
+            <!-- center icon horizontally -->
+            <div class="col-4 d-flex justify-content-center align-items-center">
+              <i class="bi bi-card-text big-icon"></i>
+            </div>
+            <div class="col-4 col-md-6 text-center d-flex align-items-center">
+              <p class="lead m-0">
+                21
+                <br />
+                Total Notes
+              </p>
             </div>
           </div>
         </div>
@@ -36,33 +40,38 @@
     </div>
     <div class="col">
       <div class="card h-100">
-        <div class="card-body d-flex align-items-center">
-          <div class="card-text">
-            <div class="row align-items-center">
-              <div class="col-4">
-                <i class="bi bi-pencil-square big-icon ms-3 me-5"></i>
-              </div>
-              <div class="col-8 text-center">
-                <p class="lead m-0">87</p>
-                <p class="lead m-0">Commited edits</p>
-              </div>
+        <div class="card-body">
+          <div class="row justify-content-center ">
+            <!-- center icon horizontally -->
+            <div class="col-4 d-flex justify-content-center align-items-center">
+              <i class="bi bi-pencil-square big-icon"></i>
+            </div>
+            <div class="col-4 col-md-6 text-center d-flex align-items-center">
+              <p class="lead m-0">
+                21
+                <br />
+                Total Edits
+              </p>
             </div>
           </div>
         </div>
       </div>
     </div>
+
     <div class="col">
       <div class="card h-100">
-        <div class="card-body d-flex align-items-center">
-          <div class="card-text">
-            <div class="row align-items-center">
-              <div class="col-4">
-                <i class="bi bi-card-checklist big-icon ms-3 me-5"></i>
-              </div>
-              <div class="col-8 text-center">
-                <p class="lead m-0">99</p>
-                <p class="lead m-0">Marked done</p>
-              </div>
+        <div class="card-body">
+          <div class="row justify-content-center ">
+            <!-- center icon horizontally -->
+            <div class="col-4 d-flex justify-content-center align-items-center">
+              <i class="bi bi-card-checklist big-icon"></i>
+            </div>
+            <div class="col-4 col-md-6 text-center d-flex align-items-center">
+              <p class="lead m-0">
+                21
+                <br />
+                Marked Done
+              </p>
             </div>
           </div>
         </div>
@@ -78,6 +87,7 @@ export default {
   // todo | add admin / users module to vuex
   // todo | add methods
   // todo | correct css layout
+  // todo | center div insede card
 };
 </script>
 
---
+++ client/src/store/modules/admin.js
@@ -0,0 +1,18 @@
+const state = {
+  allNotes: [],
+};
+
+const getters = {
+  getAllNotes: (state) => state.allNotes,
+};
+
+const actions = {};
+
+const mutations = {};
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations,
+};
